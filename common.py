59bb136290c7aa2a0c9b8813b8c1a261c6e06afe
Matthew Carruth <mcarruth@blacksky.com>
2019-12-01 22:47:42 -0800

    work, need to redo viper?

---
+++ common.py
@@ -0,0 +1,106 @@
+# painting attr: https://www.w3.org/TR/SVG/painting.html
+# svgwrite docs: https://svgwrite.readthedocs.io/en/master/
+# coords are x,y from upper-left
+
+# DIMENSIONS
+DPI = 96
+PAGE_WIDTH_PX = 816
+PAGE_HEIGHT_PX = 1056
+BUMBER_PX = 20  # printing margin
+
+# DIAGNOSTICS
+CURRENT_X = 0
+CURRENT_Y = 0
+MIN_X = 0
+MIN_Y = 0
+MAX_X = 0
+MAX_Y = 0
+
+
+def invert_y_axis(inkscape_value):
+    return PAGE_HEIGHT_PX - inkscape_value
+
+
+def convert_in_to_px(inch_value):
+    return inch_value * DPI
+
+
+def move_cursor_to(upper_left_pixel_horizontal, upper_left_pixel_vertical):
+    global CURRENT_X, CURRENT_Y
+    CURRENT_X = upper_left_pixel_horizontal
+    CURRENT_Y = upper_left_pixel_vertical
+    return [f"M {upper_left_pixel_horizontal}, {upper_left_pixel_vertical}"]
+
+
+def relative_move(upper_left_pixel_horizontal, upper_left_pixel_vertical):
+    global CURRENT_X, CURRENT_Y
+    CURRENT_X += upper_left_pixel_horizontal
+    CURRENT_Y += upper_left_pixel_vertical
+    return [f"m {upper_left_pixel_horizontal}, {upper_left_pixel_vertical}"]
+
+
+def draw_relative_line(upper_left_pixel_horizontal, upper_left_pixel_vertical):
+    global CURRENT_X, CURRENT_Y
+    CURRENT_X += upper_left_pixel_horizontal
+    CURRENT_Y += upper_left_pixel_vertical
+    return [f"l {upper_left_pixel_horizontal}, {upper_left_pixel_vertical}"]
+
+
+def draw_absolute_line(upper_left_pixel_horizontal, upper_left_pixel_vertical):
+    global CURRENT_X, CURRENT_Y
+    CURRENT_X += upper_left_pixel_horizontal
+    CURRENT_Y += upper_left_pixel_vertical
+    return [f"L {upper_left_pixel_horizontal}, {upper_left_pixel_vertical}"]
+
+
+def create_curve(start_cp_x, start_cp_y, end_cp_x, end_cp_y, end_point_x, end_point_y):
+    global CURRENT_X, CURRENT_Y
+    CURRENT_X = end_point_x
+    CURRENT_Y = end_point_y
+    return [
+        f'c {start_cp_x},{start_cp_y} '
+        f'{end_cp_x},{end_cp_y} '
+        f'{end_point_x},{end_point_y}'
+    ]
+
+
+def create_absolute_curve(start_cp_x, start_cp_y, end_cp_x, end_cp_y, end_point_x, end_point_y):
+    global CURRENT_X, CURRENT_Y
+    CURRENT_X = end_point_x
+    CURRENT_Y = end_point_y
+    return [
+        f'C {start_cp_x},{start_cp_y} '
+        f'{end_cp_x},{end_cp_y} '
+        f'{end_point_x},{end_point_y}'
+    ]
+
+
+def curve_math():
+    pass  # TODO
+
+
+def add_closure():
+    return ['z']
+
+
+def parse_svg_curve(input_str):
+    # (-*\d+\.*\d*), *(-*\d+\.*\d*) (-*\d+\.*\d*), *(-*\d+\.*\d*) (-*\d+\.*\d*), *(-*\d+\.*\d*)
+    # viper.push(create_curve($1, $2, $3, $4, $5, $6))\n
+    pass
+
+
+def move_to_calcs(start_coords, shape_offset):
+    return [start_coords[0] + shape_offset[0], start_coords[1] + shape_offset[1]]
+
+
+def scaler_calcs(start_coords, end_coords, shape_size, specific_scalars):
+    if specific_scalars[0] and specific_scalars[1]:
+        scalar_x = specific_scalars[0]
+        scalar_y = specific_scalars[1]
+    elif end_coords[0] and end_coords[1]:
+        scalar_x = (end_coords[0] - start_coords[0]) / shape_size[0]
+        scalar_y = (end_coords[1] - start_coords[1]) / shape_size[1]
+    else:
+        scalar_x = 1
+        scalar_y = 1
+    return scalar_x, scalar_y
--- svg_page2.py
+++ svg_page2.py
@@ -1,7 +1,11 @@
 import svgwrite
-from svgwrite.path import Path
 
+from svg_sheets import shapes
+from svg_sheets.common import PAGE_HEIGHT_PX, PAGE_WIDTH_PX
+from svg_sheets.shapes.viper import viper_1, viper_2
 # COLORS
+from svg_sheets.shapes.viper3 import viper_3
+
 TEXT_PRIMARY = "green"
 TEXT_SECONDARY = "green"
 
@@ -11,39 +15,28 @@ BG_SECONDARY = "green"
 LABEL_PRIMARY = "green"
 LABEL_SECONDARY = "green"
 
+
 # FONTS
 
-
-# DIMENSIONS
-DPI = 96
-PAGE_WIDTH_PX = 816
-PAGE_HEIGHT_PX = 1056
-BUMBER_PX = 20  # printing margin
-
-
-def invert_y_axis(inkscape_value):
-    return PAGE_HEIGHT_PX - inkscape_value
+def bounding_box(svg, x, y, width, height):
+    return svg.rect(insert=(x, y),
+                    size=(width, height),
+                    stroke_width="0.25",
+                    stroke="green",
+                    fill_opacity="0")
 
 
-def convert_in_to_px(inch_value):
-    return inch_value * DPI
+svg_doc = svgwrite.Drawing(filename="viper_1.svg", size=(500, 800))
 
+# svg_doc.add(shapes.test_curve())
+# svg_doc.add(viper_2(0, 0, relative_size_x=1, relative_size_y=0.7))
+# svg_doc.add(viper_2(0, 0, end_x=150, end_y=200))
 
-def rounded_rectangle(drawing, x, y, width, height):
-    return drawing.rect(insert=(x, y), size=(width, height), rx="20", ry="20",
-                        stroke_width="1",
-                        stroke="green",
-                        fill="rgb(255,255,0)")
+# testing shapes
+svg_doc.add(viper_3(0, 0, relative_size_x=1, relative_size_y=1))
+# svg_doc.add(bounding_box(svg_doc, 104, 146, 81, 58))  # TODO replace with curve math at some point
+# offset start = first x,y subtracted from shape
 
-
-def create_path():
-    return Path(
-        d="m 53.962048,239.21297 c 0,1.56896 -1.053656,3.03781 -2.343637,3.23904 -14.608462,2.27415 -29.397008,2.27415 -44.0051933,0 -1.2899845,-0.20123 -2.3439149,-1.67008 -2.3439149,-3.23904 V 68.534573 c 0,-1.569301 1.0594713,-2.854041 2.3541653,-2.854712 H 51.608159 c 1.294695,6.71e-4 2.353889,1.285411 2.353889,2.854712 z",
-        style="fill:none;fill-opacity:1;fill-rule:nonzero;stroke:#080808;stroke-width:0.38058412;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1")
-
-
-svg_doc = svgwrite.Drawing(filename="test-svgwrite.svg", size=(PAGE_WIDTH_PX, PAGE_HEIGHT_PX))
-svg_doc.add(rounded_rectangle(svg_doc, BUMBER_PX, BUMBER_PX, BUMBER_PX * 2, BUMBER_PX * 2))
-svg_doc.add(create_path())
-print(svg_doc.tostring())
 svg_doc.save()
+
+# TODO: wrap everything in layers
