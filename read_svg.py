a14814e0d21955898b189feb67591fdb35b4d4bb
Matthew Carruth <mcarruth@blacksky.com>
2019-12-13 22:00:45 -0800

    got shapes sorted

--- read_svg.py
+++ read_svg.py
@@ -2,14 +2,40 @@ from svgpathtools import disvg, svg2paths
 from svgpathtools.paths2svg import big_bounding_box
 
 
-def isolate_path(filepath_in, filename_out):
-    paths, attr_dict = svg2paths(filepath_in)
+def remove_canvas(path_list):
+    rects = []
+    for p in path_list:
+        if len(p) == 4:
+            rects.append(p)
+    for r in rects:
+        path_list.remove(r)
+    return path_list
 
-    svg_doc = disvg(paths, attributes=attr_dict, filename=filename_out, paths2Drawing=True)
+
+def remove_inkscape_attributes(attribute_dict):
+    for attr in attribute_dict:
+        keys = list(attr.keys())
+        for k in keys:
+            if 'inkscape' in k:
+                del attr[k]
+    return attribute_dict
+
+
+def break_up_paths(path_list):
+    new_paths = []
+    for p in path_list:
+        new_paths.extend(p.continuous_subpaths())
+    return new_paths
+
+
+def isolate_path(filepath_in, filename_out):
+    paths, _ = svg2paths(filepath_in)
+    paths = remove_canvas(paths)
+    paths = break_up_paths(paths)
+    svg_doc = disvg(paths, filename=filename_out, paths2Drawing=True)
     bbox = big_bounding_box(paths)
     svg_doc.viewbox(minx=bbox[0], width=bbox[1] - bbox[0], miny=bbox[2], height=bbox[3] - bbox[2])
-    # svg_doc.save()
-    svg_doc.save(pretty=True)  # TODO, work around the inkscape tag thing
+    svg_doc.save(pretty=True)
 
 
-isolate_path('/home/mcarruth/Code/personal/svg_sheets/shapes/images/viper_2.svg', 'testing.svg')
+isolate_path('./viper.svg', 'shapes/testing.svg')
--- svg_testing.py
+++ svg_testing.py
@@ -8,7 +8,97 @@ from svgwrite.path import Path
 
 from svg_sheets.common import move_cursor_to, move_to_calcs, scaler_calcs
 
-
 # TODO editor to chop up existing svg
 # TODO, colors
-# TODO, create stable samples of various shapes
+
+from math import ceil
+from os.path import join
+
+from svgpathtools import Arc, CubicBezier, Line, Path as spt_Path, QuadraticBezier, svg2paths
+from svgpathtools.paths2svg import big_bounding_box
+from svgwrite import Drawing
+
+
+def create_doc(filename, all_paths):
+    xmin, xmax, ymin, ymax = big_bounding_box(all_paths)
+    dx = xmax - xmin
+    dy = ymax - ymin
+
+    # adding a small buffer
+    # determine stroke_widths to use
+    # sw = max(dx, dy) * 1e-3
+    # stroke_widths = [sw]*len(all_paths)
+    # max_stroke_width = sw
+
+    # xmin -= 0.1 * dx + max_stroke_width / 2
+    # ymin -= 0.1 * dy + max_stroke_width / 2
+    # dx += 2 * 0.1 * dx + max_stroke_width
+    # dy += 2 * 0.1 * dy + max_stroke_width
+
+    # max dimension, if want to constrain
+    max_dim = 400
+    if dx > dy:
+        szx = str(max_dim) + 'px'
+        szy = str(int(ceil(max_dim * dy / dx))) + 'px'
+    else:
+        szx = str(int(ceil(max_dim * dx / dy))) + 'px'
+        szy = str(max_dim) + 'px'
+    dimensions = szx, szy
+
+    viewbox = f"{xmin} {ymin} {dx} {dy}"
+    return Drawing(filename=filename, size=(dx, dy), viewBox=viewbox)
+
+
+def create_path(dwg, paths, attributes):
+    for i, p in enumerate(paths):
+        if isinstance(p, spt_Path):
+            ps = p.d()
+        elif type(p) in (Line, QuadraticBezier, CubicBezier, Arc):
+            ps = spt_Path(p).d()
+        else:  # assume this path, p, was input as a Path d-string
+            ps = p
+
+        if attributes:
+            good_attribs = {'d': ps}
+            for key in attributes[i]:
+                val = attributes[i][key]
+                if key != 'd':
+                    dwg.path(ps, **{key: val})
+                    good_attribs.update({key: val})
+
+            return dwg.path(**good_attribs)
+        else:
+            return dwg.path(ps, stroke='#000000', stroke_width="1", fill='none')
+
+
+svg_dir = '/home/mcarruth/Code/personal/svg_sheets/shapes/'
+all_paths, all_attrs = svg2paths(join(svg_dir, 'bear.svg'))
+# bbox = big_bounding_box(bear_paths)
+raven_paths, raven_attr_dict = svg2paths(join(svg_dir, 'viper2.svg'))
+# bbox2 = big_bounding_box(raven_paths)
+
+all_paths.extend(raven_paths)
+all_attrs.extend(raven_attr_dict)
+svg_doc = create_doc('testing.svg', all_paths)
+p1 = create_path(svg_doc, [all_paths[0]], [all_attrs[0]])
+p2 = create_path(svg_doc, [all_paths[1]], [all_attrs[1]])
+g1 = svg_doc.g()
+g2 = svg_doc.g()
+g1.add(p1)
+g2.add(p2)
+svg_doc.add(g1)
+svg_doc.add(g2)
+
+# move, scale, etc
+g2.translate(150, 5)
+g1.scale(.8, 1)
+g2.rotate(90)
+
+# redo viewbox bound post mixins
+# TODO -
+#  could use g1.attribs.get('transform', {}).get('scale')
+#  and then do math on the bbox, but it won't account for all groups, complicated - ultimately not super needed?
+# bbox = big_bounding_box(all_paths)
+# print(bbox)
+
+svg_doc.save(pretty=True)
